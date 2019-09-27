37390a4f71a0e847d897901d701113eef4056aef
harunmohamed <harunmohamed901@gmail.com>
2019-09-28 00:50:35 +0300

    Username Valdidation Message

--- bambiv3/forms.py
+++ bambiv3/forms.py
@@ -6,7 +6,7 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationE
 from bambiv3.models import User
 
 class RegistrationForm(FlaskForm):
-	username = StringField('Username', validators = [DataRequired(), Length(min=2, max=20), Regexp(r'^[\w.-_.]+$') ])
+	username = StringField('Username', validators = [DataRequired(), Length(min=2, max=20), Regexp(r'^[\w.-_.]+$', message='No Spaces. Use "-" , "_" , "." instead') ])
 	email = StringField('Email', validators = [DataRequired(), Email()])
 	department = StringField('Department', validators=[DataRequired()])
 	student_number = StringField('Student Number', validators=[DataRequired()])
