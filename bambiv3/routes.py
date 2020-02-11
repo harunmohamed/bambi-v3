5d11f982ab8cbc7aaa9595d9174462e5c8e37e43
harunmohamed <harunmohamed901@gmail.com>
2020-02-12 00:49:24 +0200

    Can now upload images from Home Page

--- bambiv3/routes.py
+++ bambiv3/routes.py
@@ -21,16 +21,19 @@ def layout():
 	users = User.query.all()
 	return render_template('layout.html', users=users)
 
+
 @app.route('/')
 @app.route('/home', methods=['GET', 'POST'])
 def home():
 	form = PostForm()
 	if form.validate_on_submit():
-		post = Post(title=form.title.data, content=form.content.data, author=current_user)
-		db.session.add(post)
-		db.session.commit()
-		flash('Your Post Has been Created!', 'success')
-		return redirect(url_for('home'))
+		if form.image.data:
+			picture = save_picture(form.image.data)
+			post = Post(title=form.title.data, content=form.content.data, image=picture, author=current_user)
+			db.session.add(post)
+			db.session.commit()
+			flash('Your Post Has been Created!', 'success')
+			return redirect(url_for('home'))
 	page = request.args.get('page', 1, type=int)
 	posts = Post.query.order_by(Post.date_posted.desc()).paginate(per_page=50, page=page)
 	users = User.query.all()
