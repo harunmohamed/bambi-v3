1a26696dd6b9f0ac75f24b2ddc0b1056dd527e80
harunmohamed <harunmohamed901@gmail.com>
2020-02-18 21:50:39 +0200

    Reverted from alphanumeric to Regex

--- bambiv3/forms.py
+++ bambiv3/forms.py
@@ -2,11 +2,11 @@ from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError, Regexp, AlphaNumeric
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError, Regexp
 from bambiv3.models import User
 
 class RegistrationForm(FlaskForm):
-	username = StringField('Username', validators = [DataRequired(), Length(min=2, max=20), AlphaNumeric()]) #Regexp(r'^[\w.-_.]+$', message='No Spaces. Use "-" or "_" or "." instead')
+	username = StringField('Username', validators = [DataRequired(), Length(min=2, max=20), Regexp(r'^[\w.-_.]+$', message='No Spaces. Use "-" or "_" or "." instead')]) 
 	email = StringField('Email', validators = [DataRequired(), Email()])
 	department = StringField('Department', validators=[DataRequired()])
 	student_number = StringField('Student Number', validators=[DataRequired()])
