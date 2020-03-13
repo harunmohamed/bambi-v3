f6169bded6db0d4028e59698a2113310a93e870a
harunmohamed <harunmohamed901@gmail.com>
2020-03-13 18:38:34 +0200

    removed recovery email info from repo

--- bambiv3/__init__.py
+++ bambiv3/__init__.py
@@ -24,8 +24,8 @@ login_manager.login_message_category = 'info'
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'bambichatter@gmail.com' #os.environ.get('EMAIL_USER')
-app.config['MAIL_PASSWORD'] = '@bambichat' #os.environ.get('EMAIL_PASS')
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER') #set recovery email
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS') #set recovery email password
 mail = Mail(app)
 
 
