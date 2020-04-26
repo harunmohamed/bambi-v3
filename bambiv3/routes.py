a7c9b2bbab02434c5e28d347dd7dfbdf33634949
harunmohamed <harunmohamed901@gmail.com>
2020-04-26 19:45:58 +0300

    adding photo albums to profiles

--- bambiv3/routes.py
+++ bambiv3/routes.py
@@ -373,11 +373,13 @@ def user_posts(username):
 		page = request.args.get('page', 1, type=int)
 		user = User.query.filter_by(username=username).first_or_404()
 		posts = Post.query.filter_by(author=user).order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
-		return render_template('user_posts.html', posts=posts, user=user, title=user.username.title(),image_file=image_file,form=form)
+		post_images = Post.query.order_by(Post.date_posted.desc())
+		return render_template('user_posts.html', posts=posts, post_images=post_images, user=user, title=user.username.title(),image_file=image_file,form=form)
 	page = request.args.get('page', 1, type=int)
 	user = User.query.filter_by(username=username).first_or_404()
 	posts = Post.query.filter_by(author=user).order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
-	return render_template('user_posts.html', posts=posts, user=user, title=user.username.title())
+	post_images = Post.query.order_by(Post.date_posted.desc())
+	return render_template('user_posts.html', posts=posts, post_images=post_images, user=user, title=user.username.title())
 
 
 @app.route("/<string:username>", methods=['GET', 'POST'])
