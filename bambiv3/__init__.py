0e61ea0dc16c29aebdd2a588f4055941bb24e358
harunmohamed <harunmohamed901@gmail.com>
2020-06-30 23:11:01 +0300

    routine logging

--- bambiv3/__init__.py
+++ bambiv3/__init__.py
@@ -8,6 +8,7 @@ from flask_migrate import Migrate
 from flask_moment import Moment
 from flask_bootstrap import Bootstrap
 from flask_pagedown import PageDown
+from flask_socketio import SocketIO, emit
 
 
 app = Flask(__name__)
@@ -20,6 +21,7 @@ bcrypt = Bcrypt(app)
 moment = Moment(app)
 bootstrap = Bootstrap(app)
 pagedown = PageDown(app)
+socketio = SocketIO(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
